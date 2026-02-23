7d622cb4a2b2a03f4b1cce41a886d1a14d441d4f
Marvin Oey <marvin.oey@optimizely.com>
2026-02-23 14:27:45 -0500

    Add codegen

---
+++ codegen.ts
@@ -0,0 +1,28 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import {loadEnv} from 'vite';
+
+const {OPTIMIZELY_GRAPH_GATEWAY, OPTIMIZELY_GRAPH_SINGLE_KEY} = loadEnv('', process.cwd(), '');
+ 
+const config: CodegenConfig = {
+  schema: OPTIMIZELY_GRAPH_GATEWAY + '/content/v2?auth=' + OPTIMIZELY_GRAPH_SINGLE_KEY,
+  documents: ['src/**/*.ts'],
+  ignoreNoDocuments: true,
+  generates: {
+    '__generated/graphql/': {
+      preset: 'client',
+      config: {
+        documentMode: 'string',
+        useTypeImports: true
+      }
+    },
+    '__generated/schema.graphql': {
+      plugins: ['schema-ast'],
+      config: {
+        includeDirectives: true,
+        useTypeImports: true,
+      }
+    }
+  }
+}
+ 
+export default config
